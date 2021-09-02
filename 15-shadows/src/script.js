4d6a0753ffc9706e6b07938f3cb5f56087c39c4a
Rohit Kaushik <rkaushik@lixar.com>
2021-09-02 10:31:09 -0400

    enable shadows

--- 15-shadows/src/script.js
+++ 15-shadows/src/script.js
@@ -47,6 +47,7 @@ const sphere = new THREE.Mesh(
     new THREE.SphereGeometry(0.5, 32, 32),
     material
 )
+sphere.castShadow = true
 
 const plane = new THREE.Mesh(
     new THREE.PlaneGeometry(5, 5),
@@ -54,6 +55,7 @@ const plane = new THREE.Mesh(
 )
 plane.rotation.x = - Math.PI * 0.5
 plane.position.y = - 0.5
+plane.receiveShadow = true
 
 scene.add(sphere, plane)
 
@@ -102,6 +104,7 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.shadowMap.enabled = true
 
 /**
  * Animate
