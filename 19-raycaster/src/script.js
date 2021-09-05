09c72c9eec3f24dc4c114f4ff4927efed911aa09
rkaushik29 <rohit.rk.rk1@gmail.com>
2021-09-04 20:33:10 -0400

    ray intersect

--- 19-raycaster/src/script.js
+++ 19-raycaster/src/script.js
@@ -18,13 +18,9 @@ const scene = new THREE.Scene()
 /**
  * Raycaster
  */
- const raycaster = new THREE.Raycaster()
+const raycaster = new THREE.Raycaster()
 
-const rayOrigin = new THREE.Vector3(- 3, 0, 0)
-const rayDirection = new THREE.Vector3(10, 0, 0)
-rayDirection.normalize()
 
-raycaster.set(rayOrigin, rayDirection)
 
 /**
  * Objects
@@ -100,6 +96,30 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Cast a ray
+    const rayOrigin = new THREE.Vector3(- 3, 0, 0)
+    const rayDirection = new THREE.Vector3(1, 0, 0)
+    rayDirection.normalize()
+
+    raycaster.set(rayOrigin, rayDirection)
+
+    const objectsToTest = [object1, object2, object3]
+    const intersects = raycaster.intersectObjects(objectsToTest)
+
+    for(const object of objectsToTest)
+    {
+        object.material.color.set('#ff0000')
+    }
+    for(const intersect of intersects)
+    {
+        intersect.object.material.color.set('#0000ff')
+    }
+
+    // Animate objects
+    object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5
+    object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5
+    object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5
 
     // Update controls
     controls.update()
