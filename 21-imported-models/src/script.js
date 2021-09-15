1374d405a4954d2be4ec1f262bf67b10ff2275d1
rkaushik29 <rohit.rk.rk1@gmail.com>
2021-09-15 11:52:39 -0400

    import loader

--- 21-imported-models/src/script.js
+++ 21-imported-models/src/script.js
@@ -2,6 +2,7 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 /**
  * Base
@@ -14,6 +15,9 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+
+// Models
+const gltfLoader = new GLTFLoader()
 
 /**
  * Floor
